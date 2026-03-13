a7221de0c653d9ed6a8be7e51d4979fabfd29845
sarevaloq01 <sarevaloq01@gmail.com>
2026-03-13 21:34:00 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 def test_hamburguesa():
     ingrediente = "carne"
-    assert ingrediente == "carne", "¡Falta la carne en la hamburguesa!"
+    assert ingrediente == "piedra", "¡Falta la carne en la hamburguesa!"
     print("Test superado: La hamburguesa tiene carne.")
 
 if __name__ == "__main__":
